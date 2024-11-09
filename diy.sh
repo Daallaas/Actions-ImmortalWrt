894fc86e0e33c3e7ebdccade615a216677f8aa27
Daallaas <120786231+Daallaas@users.noreply.github.com>
2024-11-09 18:59:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,7 +1,7 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-rm -rf feeds/packages/net/v2ray-geodata
+# drop mosdns and v2ray-geodata packages that come with the source
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
