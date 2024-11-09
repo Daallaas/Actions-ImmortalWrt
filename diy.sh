5929551600310bf95b9734787c66b80b368c2dc2
Daallaas <120786231+Daallaas@users.noreply.github.com>
2024-11-09 16:59:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,7 @@
 # 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
-# git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
-# git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
